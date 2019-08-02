92d40c4e4f5fde5d66d7a2a8472df6b32698a78e
RY lin <1019022357@qq.com>
2019-08-02 10:54:45 +0800

    z

---
+++ routes/chance.js
@@ -0,0 +1,12 @@
+const router = require('koa-router')();
+
+router.prefix('/change');
+
+router.get('/', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!'
+  });
+});
+
+
+module.exports = router;
